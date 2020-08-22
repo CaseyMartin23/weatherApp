8043177ef3cd0ee8c5a997e4a40b5469b7f09e09
CaseyMartin23 <cm232311@gmail.com>
2020-08-23 00:00:59 +0200

    Routing system added

--- front-end/src/components/App.tsx
+++ front-end/src/components/App.tsx
@@ -1,9 +1,23 @@
 import React from "react";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "../pages/home";
+import Guide from "../pages/guide";
 import "../App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App"></div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/guide">
+          <Guide />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
